a81f5f80d069439e3306bf7931dd9b6c1943e7e5
Romaldokuznecov <143839311+Romaldokuznecov@users.noreply.github.com>
2025-10-16 11:40:51 +0300

    Add files via upload

---
+++ a+b.cpp
@@ -0,0 +1,21 @@
+/******************************************************************************
+
+                              Online C++ Compiler.
+               Code, Compile, Run and Debug C++ program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+
+int32_t main()
+{
+	int a;
+	cin>>a;
+	int b;
+	cin>>b;
+	cout<<a+b<<"\n";
+    return 0;
+}
