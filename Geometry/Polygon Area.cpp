9a9805c144d6ef8d4a542b034a44c8d0e9648446
Romaldokuznecov <143839311+Romaldokuznecov@users.noreply.github.com>
2025-10-19 18:47:03 +0300

    Create Polygon Area.cpp

---
+++ Geometry/Polygon Area.cpp
@@ -0,0 +1,9 @@
+int D_area_polygon(vector<pt> &polygon){
+    pt zero; zero.x=0; zero.y=0;
+    int ans=0; 
+    int n=polygon.size();
+    for(int i=0;i<n;i++){
+        ans+=D_area(zero, polygon[i], polygon[(i+1)%n]);
+    }
+    return ans;
+}//с точностью до знака (удвоенная!)
