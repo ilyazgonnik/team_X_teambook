cb87c91a13490e9b183e9c7e6ca71a52b81c7e46
Romaldokuznecov <143839311+Romaldokuznecov@users.noreply.github.com>
2025-10-21 21:17:47 +0300

    Create Convex Hull.cpp

---
+++ Geometry/Convex Hull.cpp
@@ -0,0 +1,23 @@
+#define all(x) (x).begin(),(x).end()
+using pt = pair<int, int>;
+#define x first
+#define y second
+
+int cross(pt p, pt q) { return p.x * q.y - p.y * q.x; }
+
+pt operator-(pt a, pt b) { return {a.x - b.x, a.y - b.y}; }
+
+vector<pt> convex(vector<pt> a) {
+  sort(all(a));
+  a.erase(unique(all(a)), a.end());
+  vector<pt> h;
+  for (int t = 0; t < 2; ++t) {
+    int sz = h.size() - t;
+    for (auto p: a) {
+      while (h.size() >= sz + 2 && cross(p - h.end()[-1], h.end()[-2] - h.end()[-1]) <= 0) h.pop_back();
+      h.push_back(p);
+    }
+    reverse(all(a));
+  }
+  return h; // h is circular: h.empty() || h.front() == h.back()
+}
