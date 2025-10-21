d1c18f10a569108011928e238ab2a3c235061f2a
Romaldokuznecov <143839311+Romaldokuznecov@users.noreply.github.com>
2025-10-21 20:32:44 +0300

    Update Chinese Remainder Theorem.cpp

--- Number Theory/Chinese Remainder Theorem.cpp
+++ Number Theory/Chinese Remainder Theorem.cpp
@@ -1,12 +1,12 @@
-int gcd(int a, int b, int &x, int &y) {
+int lin_gcd(int a, int b, int &x, int &y) {
   if (b==0) { x = 1; y = 0; return a; }
-  int d = gcd(b,a%b,y,x);
+  int d = lin_gcd(b,a%b,y,x);
   y-=a/b*x;
   return d;
 }
 int inv(int r, int m) {
   int x, y;
-  gcd(r,m,x,y);
+  lin_gcd(r,m,x,y);
   return (x+m)%m;
 }
 int crt(int r_n, int n, int r_m, int m) { return r_n + ((r_m - r_n) % m + m) * inv(n, m) % m * n; }
