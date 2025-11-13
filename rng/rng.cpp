c8de0443602fef0688f5b2f0d4ceca94662192bf
ilyazgonnik <113089818+ilyazgonnik@users.noreply.github.com>
2025-11-14 00:43:11 +0300

    Update rng.cpp

--- rng/rng.cpp
+++ rng/rng.cpp
@@ -1,4 +1,4 @@
 using uid = std::uniform_int_distribution<int>;
 mt19937_64 rng(chrono::steady_clock::now().time_since_epoch().count());
-//using uid = std::uniform_real_distribution<double>; для вещественных
-uid(0,n)(rng); //границы включительно
+//using uid = std::uniform_real_distribution<double>; for real
+uid(0,n)(rng); //bounds included
