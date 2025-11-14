7733e1d5df4e2fb6c3301b0de0681deb85c0a19a
Romaldokuznecov <143839311+Romaldokuznecov@users.noreply.github.com>
2025-11-14 14:14:35 +0300

    Update LCA.cpp

--- Graphs/Graphs/LCA.cpp
+++ Graphs/Graphs/LCA.cpp
@@ -1,6 +1,6 @@
-int l;//определить величины так: l = 1; while ((1<<l) <= n){l++;}
-vector < vector<int> > up;//определить величины так: for (int i=0; i<n; ++i){  up[i].resize (l+1);}
-//бинподъем up[i][j]=2^j-ый предок вершины i 
+int l;//define values (in main?): l = 1; while ((1<<l) <= n){l++;}
+vector < vector<int> > up;//define values (in main?): for (int i=0; i<n; ++i){  up[i].resize (l+1);}
+//bin_up:: up[i][j]=2^j-th parent of i 
 void dfs_bin_up (int v, int p = 0) {
 	time_in[v] = timer++;
 	up[v][0] = p;
@@ -18,7 +18,7 @@ void dfs_bin_up (int v, int p = 0) {
 
 bool upper (int a, int b) {
 	return time_in[a] <= time_in[b] && time_in[b]<time_out[a];
-}//a - предок b
+}//a - (some of) parent b
 
 int lca (int a, int b) {
 	if (upper (a, b))  return a;
@@ -30,4 +30,4 @@ int lca (int a, int b) {
 	}
 	return up[a][0];
 }
-//использовать:dfs_bin_up(0);
+//!!!don't forget to use:dfs_bin_up(0);
