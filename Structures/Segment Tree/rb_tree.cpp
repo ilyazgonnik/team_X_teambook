2abe628419f2a040ab46b16340cef7630e55330a
Romaldokuznecov <143839311+Romaldokuznecov@users.noreply.github.com>
2025-11-14 18:14:23 +0300

    Update rb_tree.cpp

--- Structures/Segment Tree/rb_tree.cpp
+++ Structures/Segment Tree/rb_tree.cpp
@@ -7,22 +7,22 @@ using namespace std;
 typedef tree<int, null_type, less<int>, rb_tree_tag,
              tree_order_statistics_node_update> ordered_set;
 
-int main() {
+/*int main() {
     ordered_set s;
 
-    // Вставка элементов
+    // Inserting elements
     s.insert(10);
     s.insert(5);
     s.insert(20);
     s.insert(15);
 
-    // Найти k-й наименьший элемент (0-based)
+    // Find k-th snallest element (0-based)
     cout << *s.find_by_order(0) << endl; // 5
     cout << *s.find_by_order(2) << endl; // 15
 
-    // Сколько элементов меньше чем X
+    // How many elements are less than X
     cout << s.order_of_key(12) << endl; // 2 (5, 10 < 12)
     cout << s.order_of_key(15) << endl; // 2 (5, 10 < 15)
-
+  
     return 0;
-}
+}*/
