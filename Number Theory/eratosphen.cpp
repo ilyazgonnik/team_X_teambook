829d75eba3be55732bf5e4c64ca4c09756a1fba4
ilyazgonnik <113089818+ilyazgonnik@users.noreply.github.com>
2025-11-14 13:10:55 +0300

    Create eratosphen.cpp

---
+++ Number Theory/eratosphen.cpp
@@ -0,0 +1,16 @@
+void sieve_eratosthenes(int n, vector<int>& pr) {
+    vector<bool> is_prime(n + 1, true);
+    is_prime[0] = is_prime[1] = false;
+    
+    for (int i = 2; i <= n; ++i) {
+        if (is_prime[i]) {
+            pr.push_back(i);
+           
+            if (i * i <= n) {
+                for (int j = i * i; j <= n; j += i) {
+                    is_prime[j] = false;
+                }
+            }
+        }
+    }
+}
