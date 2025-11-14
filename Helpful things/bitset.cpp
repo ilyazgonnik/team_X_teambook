dae46d59cac9bc60e380734b54983eb093c15f9d
Romaldokuznecov <143839311+Romaldokuznecov@users.noreply.github.com>
2025-11-14 23:34:17 +0300

    Update bitset.cpp

--- Helpful things/bitset.cpp
+++ Helpful things/bitset.cpp
@@ -1,1 +1,12 @@
-code_speed/=64;
+bitset<9> a;
+bitset<9>b;
+a.flip(3);
+bitset<9>c=a&b, a|b, a^b;
+~ inverse all bites
+<< shifts left (save in new bitset) (<<= for save in same value)
+>> shifts right (>>= ...)
+a.test(4) - check this bit
+a.set(2) - set the value true
+a.reset(2) - reset the value false
+a.count() - how many truths
+a.any() - is the someone true?
