fa4e2cb77818f8382c2617673140d68f12f9e7ae
Romaldokuznecov <143839311+Romaldokuznecov@users.noreply.github.com>
2025-11-14 14:04:31 +0300

    Update dfs.cpp

--- Graphs/dfs.cpp
+++ Graphs/dfs.cpp
@@ -8,3 +8,8 @@ void dfs(int u){
     }
     time_out[u]=timer;
 }
+//u is (some of) parent v iff time_in[v] \in [time_in[u], time_out[u]) 
+// intervals [time_in, time_out) are not intersecting or are nesting
+//time_in is permutation of [0, n)
+//size of subtree from v (including v) = time_out[v]-time_in[v]
+//indexes of subtrees are intervals in time_in vector. 
