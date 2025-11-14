de543e53ed48312d94021061aa6e6c4f99f92e66
Romaldokuznecov <143839311+Romaldokuznecov@users.noreply.github.com>
2025-11-14 13:26:18 +0300

    Update eratosphen.cpp

--- Number Theory/eratosphen.cpp
+++ Number Theory/eratosphen.cpp
@@ -1,11 +1,9 @@
-void sieve_eratosthenes(int n, vector<int>& pr) {
+void sieve_eratosthenes(int n) {
     vector<bool> is_prime(n + 1, true);
     is_prime[0] = is_prime[1] = false;
-    
     for (int i = 2; i <= n; ++i) {
         if (is_prime[i]) {
             pr.push_back(i);
-           
             if (i * i <= n) {
                 for (int j = i * i; j <= n; j += i) {
                     is_prime[j] = false;
